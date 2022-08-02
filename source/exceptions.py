9bc774ae4516e6deb2b5d22c07982ff6f39324f1
Selim Raboudi <selim.raboudi@gmail.com>
2022-08-02 18:35:07 +0200

    add custom exception for Type error

---
+++ source/exceptions.py
@@ -0,0 +1,11 @@
+from string import Template
+
+
+class TypeError(Exception):
+    def __init__(self, var_name: str, given_type: type, expected_type: type|str):
+        self.var_name = var_name
+        self.given_type = given_type
+        self.expected_type = expected_type
+        t = Template("variable '$v' type is not valid.\ntype: $g\nexpected type: $e")
+        msg = t.substitute(v=self.var_name, g=self.given_type, e=self.expected_type)
+        super().__init__(msg)
