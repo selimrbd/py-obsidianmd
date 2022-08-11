6ba777e4f8a393c1e706736a00f3486406e753b4
Selim Raboudi <selim.raboudi@gmail.com>
2022-08-11 15:10:08 +0200

    remove erase tests

--- test/metadata/test_frontmatter.py
+++ test/metadata/test_frontmatter.py
@@ -4,7 +4,6 @@ from ..load_test_data import load_data
 from .templates import (
     add_test_function_metadata,
     parse_name_function_tested,
-    t_erase,
     t_exists,
     t_parse,
     t_to_string,
@@ -14,7 +13,7 @@ from .templates import (
 PATH_TEST_DEF = Path(__file__).parent / "test_Frontmatter.json"
 data = load_data(PATH_TEST_DEF)
 
-for t_fn in [t_parse, t_to_string, t_update_content, t_exists, t_erase]:
+for t_fn in [t_parse, t_to_string, t_update_content, t_exists]:
     name_f = parse_name_function_tested(t_fn.__name__)
     test_ids: list[str] = list(data["tests"][f"tests-{name_f}"].keys())
     for tid in test_ids:
